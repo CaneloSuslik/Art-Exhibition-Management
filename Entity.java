fb536586800539e493db3aa580cc026bfb40de51
CaneloSuslik <sultansmailov@icloud.com>
2025-01-10 13:21:18 +0500

    Update and rename Exhibit.java to Entity.java

---
+++ Entity.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public abstract class Entity {
+    private int id;
+
+    public Entity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Entity entity = (Entity) o;
+        return id == entity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
