faaa986dbb3bb39e0935e920bfea0beb06f62037
CaneloSuslik <sultansmailov@icloud.com>
2025-01-10 12:31:14 +0500

    Update Artist.java

--- Artist.java
+++ Artist.java
@@ -1,42 +1,17 @@
-// Artist.java
-class Artist { 
-    private String name;
+public class Artist extends Person {
     private String style;
 
     public Artist(String name, String style) {
-        this.name = name;
+        super(name);
         this.style = style;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getStyle() {
         return style;
     }
 
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
     @Override
     public String toString() {
-        return "Artist{" +
-                "name='" + name + '\'' +
-                ", style='" + style + '\'' +
-                '}';
+        return "Artist{name='" + name + "', style='" + style + "'}";
     }
 }
-
-public class Main { 
-    public static void main(String[] args) {
-        Artist artist = new Artist("Pablo Picasso", "Cubism");
-        System.out.println(artist);
-    }
-}
-
