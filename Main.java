6e645d22de502515218b8846d7554c14505e5e47
CaneloSuslik <sultansmailov@icloud.com>
2025-01-10 13:23:28 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,32 +3,32 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем художников
+        
         Artist artist1 = new Artist(1, "Vincent van Gogh", "Post-Impressionism");
         Artist artist2 = new Artist(2, "Claude Monet", "Impressionism");
 
-        // Создаем работы
+        
         Artwork artwork1 = new Artwork(1, "Starry Night", "Oil Painting", 1000000.0);
         Artwork artwork2 = new Artwork(2, "Sunflowers", "Oil Painting", 800000.0);
         Artwork artwork3 = new Artwork(3, "Water Lilies", "Oil Painting", 1200000.0);
 
-        // Создаем галерею
+        
         Gallery gallery = new Gallery("MoMA", "New York");
         gallery.addArtwork(artwork1);
         gallery.addArtwork(artwork2);
         gallery.addArtwork(artwork3);
 
-        // Сортировка работ по цене
+        
         List<Artwork> sortedArtworks = gallery.getArtworks().stream()
                 .sorted(Comparator.comparing(Artwork::getPrice))
                 .collect(Collectors.toList());
 
-        // Фильтрация работ по цене (больше 1 млн)
+        
         List<Artwork> expensiveArtworks = gallery.getArtworks().stream()
                 .filter(artwork -> artwork.getPrice() > 1000000)
                 .collect(Collectors.toList());
-
-        // Вывод результатов
+        
+        
         System.out.println("Артисты:");
         System.out.println(artist1);
         System.out.println(artist2);
@@ -45,7 +45,7 @@ public class Main {
         System.out.println("\nРаботы дороже 1 млн:");
         expensiveArtworks.forEach(System.out::println);
 
-        // Пример использования Address
+        
         Address galleryAddress = new Address("New York", "11 W 53rd St");
         System.out.println("\nАдрес галереи:");
         System.out.println(galleryAddress);
