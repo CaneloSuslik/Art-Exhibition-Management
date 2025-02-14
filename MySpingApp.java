ef29406ed2a7d5177b3ee1da831035b6c70fde83
CaneloSuslik <sultansmailov@icloud.com>
2025-02-14 15:05:19 +0500

    Create MySpingApp.java

---
+++ MySpingApp.java
@@ -0,0 +1,11 @@
+package com.example.myspringapp; // Задай правильный пакет
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApp.class, args);
+    }
+}
